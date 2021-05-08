eb2cde0d288c797d130a07cf2fd4ec2405fee32c
Jason Christian H <nakamarujc@gmail.com>
2021-05-09 01:55:46 +0700

    Completed initial version of homepage without the interactive man

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '.splide', {
+        type: 'loop',
+        height: "56.25%",
+        width: "100%",
+        focus: 'center',
+        trimSpace: false,
+        gap: "5vw",
+        autoplay: true,
+        cover: true,
+    } ).mount();
+
+    var elems = document.querySelectorAll('.carousel');
+    var cars = M.Carousel.init(elems, {
+        duration: 50,
+    });
+
+    setInterval(() => {
+        cars[0].next();
+    }, 3000);
+});
